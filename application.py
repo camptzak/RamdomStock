a95a999023c54196a49218ad1a077879df6e5cdd
camptzak <zackpowers@gmail.com>
2020-08-01 22:44:44 -0400

    080120

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import sqlite3
 import random
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
