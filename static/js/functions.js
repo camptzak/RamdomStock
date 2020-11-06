b3e068472f8f0ae93ead7c18f873c7a0351c2fad
camptzak <zackpowers@gmail.com>
2020-11-06 16:20:32 -0500

    Several Blog Updates. Twitter Bot Update. Follow Bot Update

---
+++ static/js/functions.js
@@ -0,0 +1,166 @@
+// share button functions
+function twitter(){
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("http://twitter.com/share?text=" + "ACT Symbol: " + symbol + "%0a" + "Company: " + company + "%0a" + "&url=" + currentPageUrl + "&hashtags=RandomStock")
+}
+
+function facebook() {
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("https://www.facebook.com/sharer/sharer.php?u=" + currentPageUrl + "&quote= ACT Symbol: " + symbol + "%0a" + "Company: " + company)
+}
+
+function instagram() {
+  window.open("https://www.instagram.com")
+}
+
+function reddit() {
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("http://www.reddit.com/submit?url=ACT Symbol: " + symbol + "%0a" + "Company: " + company + "%0a" +"randomstock.net%0a&title=Random%20Stock")
+}
+
+function linkedin() {
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("http://www.linkedin.com/feed")
+}
+
+// coin javascript functions
+
+function twitterCoin(){
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("http://twitter.com/share?text=" + "Symbol: " + symbol + "%0a" + "Coin: " + company + "%0a" + "&url=" + currentPageUrl + "&hashtags=RandomStock")
+}
+
+function facebookCoin() {
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("https://www.facebook.com/sharer/sharer.php?u=" + currentPageUrl + "&quote=Symbol: " + symbol + "%0a" + "Coin: " + company)
+}
+
+function instagramCoin() {
+  window.open("https://www.instagram.com")
+}
+
+function redditCoin() {
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("http://www.reddit.com/submit?url=Symbol: " + symbol + "%0a" + "Coin: " + company + "%0a" +"randomstock.net%0a&title=Random%20Stock")
+}
+
+function linkedinCoin() {
+  let symbol = document.getElementById("symbol").innerHTML;
+  let company = document.getElementById("company").innerHTML;
+  const currentPageUrl = window.location.href;
+  window.open("http://www.linkedin.com/feed")
+}
+
+
+// Asynchronous Buttons
+function indexButton() {
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+       var response = JSON.parse(xhttp.responseText)
+       document.getElementById("symbol").innerHTML = response.symbol;
+       document.getElementById("company").innerHTML = response.company;
+       document.getElementById("exchange").innerHTML = response.exchange;
+
+       document.getElementById("analysisIndex").href = foo
+    }
+};
+
+xhttp.open("GET", "/_indexButton", true);
+xhttp.send();
+}
+
+
+function pennyButton() {
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+       var response = JSON.parse(xhttp.responseText)
+       document.getElementById("symbol").innerHTML = response.symbol;
+       document.getElementById("company").innerHTML = response.company;
+    }
+};
+xhttp.open("GET", "/_pennyButton", true);
+xhttp.send();
+}
+
+function cryptoButton() {
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+       var response = JSON.parse(xhttp.responseText)
+       document.getElementById("symbol").innerHTML = response.symbol;
+       document.getElementById("company").innerHTML = response.company;
+       document.getElementById("lookup").innerHTML = response.lookup;
+    }
+};
+xhttp.open("GET", "/_cryptoButton", true);
+xhttp.send();
+}
+
+
+// Stock Quote script
+function stockQuote() {
+    let exchange = document.getElementById("exchange").innerText;
+
+
+    if (exchange == 'AMEX'){
+        let symb = document.getElementById("symbol").innerText;
+        window.open("https://www.tradingview.com/symbols/AMEX-" + symb)
+    }
+
+    else if (exchange == 'LSE'){
+        let symb = document.getElementById("symbol").innerHTML;
+        window.open("https://www.tradingview.com/symbols/LSE-" + symb)
+    }
+
+    else if (exchange == 'NASDAQ'){
+        let symb = document.getElementById("symbol").innerHTML;
+        window.open("https://www.tradingview.com/symbols/NASDAQ-" + symb)
+    }
+
+    else if (exchange == 'NYSE'){
+        let symb = document.getElementById("symbol").innerHTML;
+        window.open("https://www.tradingview.com/symbols/NYSE-" + symb)
+    }
+
+    else if (exchange == 'OTCBB'){
+        let symb = document.getElementById("symbol").innerHTML;
+        window.open("https://www.tradingview.com/symbols/OTC-" + symb)
+    }
+
+    else if (exchange == 'SGX'){
+        let symb = document.getElementById("symbol").innerHTML;
+        window.open("https://www.tradingview.com/symbols/SGX-:" + symb)
+    }
+
+    else if (exchange == 'Coinbase'){
+        let lookup = document.getElementById("lookup").innerHTML;
+        window.open("https://www.coinbase.com/price/" + lookup)
+    }
+    else{
+        window.open("https://www.nyse.com")
+    }
+
+
+}
