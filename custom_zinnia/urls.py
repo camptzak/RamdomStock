1e1b695605ab86a6510057bf68dbed2aed37aeb2
muneebahmad <muneebahmad222@gmail.com>
2021-01-29 21:03:16 +0500

    [Zinnia URLS]

--- custom_zinnia/urls.py
+++ custom_zinnia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('weblog/', include('zinnia.urls')),
     path('blog/', views.Blog.as_view(), name='blog'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blogDetails'),
+    path('weblog/', include('zinnia.urls')),
 ]
